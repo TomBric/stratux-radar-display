901eb04b1d42d1edf87d62d8ab119313fedcdd28
TomBric <47101971+TomBric@users.noreply.github.com>
2021-01-06 13:18:39 +0100

    Changed RETRY time to 1 sec

--- image/configure_radar.sh
+++ image/configure_radar.sh
@@ -6,21 +6,20 @@
 #enable usermod
 # sudo usermod -a -G spi,gpio,i2c pi
 
-#apt-get dist-update
 
 #luma files and more
-apt-get update
-apt-get install python3-pip python3-pil
+apt update && apt full-upgrade -y
+apt-get install python3-pip python3-pil -y
 apt-get install libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 -y
 pip3 install luma.oled
-apt-get install git
+apt-get install git -y
 
 
 #websockets for radar
 pip3 install websockets
 
 # espeak-ng for sound output
-apt install espeak-ng espeak-ng-data libespeak-ng-dev
+apt install espeak-ng espeak-ng-data libespeak-ng-dev -y
 pip3 install py-espeak-ng
 
 # get files from repo
