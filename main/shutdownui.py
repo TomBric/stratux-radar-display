fe887282c374e19412d4328d7a66825bd730101e
TomBric <47101971+TomBric@users.noreply.github.com>
2021-01-25 15:19:22 +0100

    Shutdown via button added

--- main/shutdownui.py
+++ main/shutdownui.py
@@ -58,7 +58,7 @@ def draw_shutdown(draw, display_control):
         return False
 
 
-async def user_input(display_time):
+def user_input(display_time):
     global shutdown_time
     global clear_before_shutoff
 
@@ -74,7 +74,7 @@ async def user_input(display_time):
         print("Shutdown now")
         clear_before_shutoff = True   # enable display driver to shut off
         print("Await ", display_time*3, " secs")
-        await asyncio.sleep(display_time*3)  # time for the display to shut off
+        asyncio.sleep(display_time*3)  # time for the display to shut off
         # os.popen("sudo shutdown --poweroff now").read()
         shutdown_time = 0.0
         return 1  # go back to radar mode; but never reached normally
