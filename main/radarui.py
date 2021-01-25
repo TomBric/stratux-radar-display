bee51b0a38cede94050e22d38ca838ee160d8597
TomBric <47101971+TomBric@users.noreply.github.com>
2021-01-25 15:16:52 +0100

    Shutdown via button added

--- main/radarui.py
+++ main/radarui.py
@@ -73,7 +73,7 @@ def user_input(rrange, rlimits):   # return Nextmode, toogleSound  (Bool)
     if btime == 0:
         return 1, False
     if button == 0:
-        if btime == 2:    # left and lon
+        if btime == 2:    # left and long
             print("Next mode is shutdown")
             return 3, False  # start next mode shutdown!
         else:          # left and short
