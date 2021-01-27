580298c05b979e55bfe6291887d7f1d91fe96c7b
TomBric <47101971+TomBric@users.noreply.github.com>
2021-01-27 14:03:58 +0100

    Refresh on right button for epaper

--- main/radar.py
+++ main/radar.py
@@ -350,6 +350,7 @@ async def user_interface():
         elif global_mode == 3:  # shutdown mode
             next_mode = shutdownui.user_input()
         elif global_mode == 4:  # refresh mode
+            print("UI driver: Refresh mode")
             await asyncio.sleep(UI_REACTION_TIME*2)   # give display driver time ...
 
         if next_mode > 0:
