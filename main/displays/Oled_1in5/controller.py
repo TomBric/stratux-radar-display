500e39484d2a6879fd54abb28a54961004e4d8a5
TomBric <47101971+TomBric@users.noreply.github.com>
2021-02-02 16:55:53 +0100

    Starting AHRS

--- main/displays/Oled_1in5/controller.py
+++ main/displays/Oled_1in5/controller.py
@@ -356,7 +356,7 @@ def ahrs(draw, pitch, roll, heading, slipskid):
     h1, h2 = linepoints(pitch, roll, 0, 200)  # horizon points
     draw.line((h1, h2), fill="white", width=2)  # horizon line
     h3, h4 = linepoints(pitch, roll, -180, 200)
-    draw.polygon((h1, h2, h3, h4)), fill="blue")  # sky
+    draw.polygon((h1, h2, h3, h4), fill="blue")  # sky
     h3, h4 = linepoints(pitch, roll, 180, 200)
     draw.polygon((h1, h2, h3, h4), fill="blue")  # earth
 
