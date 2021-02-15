c834a5bd7036d509184d5b83bf73b833edf197d6
TomBric <47101971+TomBric@users.noreply.github.com>
2021-02-15 10:45:06 +0100

    Improving refresh speed for Epaper

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -158,7 +158,7 @@ def refresh():
     print("Refreshing display ...")
     # device.init(0)
     # device.Clear(0x00, 0)  # necessary to overwrite everything
-    # device.Clear(0xFF, 0)  # necessary to overwrite everything
+    device.Clear(0xFF, 0)  # necessary to overwrite everything
     device.init(1)
     # device.Clear(0xFF, 1)
     print("Done.")
