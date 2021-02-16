2ef12446c57de16175780325ef15c78831337cbd
TomBric <47101971+TomBric@users.noreply.github.com>
2021-02-16 08:18:05 +0100

    Improving refresh speed for Epaper, using numpy

--- main/displays/Epaper_3in7/epd3in7.py
+++ main/displays/Epaper_3in7/epd3in7.py
@@ -245,7 +245,7 @@ class EPD:
 
 
     def getbuffer_optimized(self, image):
-        pb = numpy.packbits(numpy.array(image),1)
+        pb = numpy.packbits(numpy.array(image))
         return pb
         '''
         # logging.debug("bufsiz = ",int(self.width/8) * self.height)
