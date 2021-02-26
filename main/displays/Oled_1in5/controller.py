840031569172a48afec6977f5c537f1668968209
TomBric <47101971+TomBric@users.noreply.github.com>
2021-02-26 21:34:15 +0100

    Start of interactive network config

--- main/displays/Oled_1in5/controller.py
+++ main/displays/Oled_1in5/controller.py
@@ -366,9 +366,9 @@ def screen_input(draw, headline, subline, text, left, middle, right, prefix, inp
     bbox = draw.textbbox((bbox[0], bbox[3]), prefix, font=smallfont)
     draw.text((bbox[0], bbox[3]), prefix, fill="white", font=smallfont)
 
-    bbox = draw.textbbox((bbox[2], bbox[3]), inp, font=smallfont)
+    bbox = draw.textbbox((bbox[2], bbox[1]), inp, font=smallfont)
     draw.rectangle(bbox, fill="red")
-    draw.text((bbox[0], bbox[3]), inp, font=smallfont, fill="white")
+    draw.text((bbox[2], bbox[1]), inp, font=smallfont, fill="white")
     draw.text((bbox[2], bbox[3]), suffix, font=smallfont, fill="white")
 
     draw.text((0, sizey - SMALL - 3), left, font=smallfont, fill="green")
