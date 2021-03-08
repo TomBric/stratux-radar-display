31619ec31caba9ba4341e29210f6b28c9fd208ec
TomBric <47101971+TomBric@users.noreply.github.com>
2021-03-08 12:56:59 +0100

    Reboot triggered after network reconfig

--- main/statusui.py
+++ main/statusui.py
@@ -315,8 +315,7 @@ def set_network(wifi, passw, new_stratux):
         logging.debug("STATUSUI: Setting Wifi network failed.")
         return
     logging.debug("STATUSUI: Rebooting!")
-    display.cleanup()
-    os.system('sudo reboot')
+    os.system('sleep 2; sudo reboot')
     if res != 0:
         logging.debug("STATUSUI: Reboot attempt failed.")
 
