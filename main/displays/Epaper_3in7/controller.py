11d52cfd37d9b06b8122bc0d83901eb5e2807ea1
TomBric <47101971+TomBric@users.noreply.github.com>
2021-03-13 10:32:52 +0100

    Start: Epaper with optional display of registration

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -201,9 +201,9 @@ def aircraft(draw, x, y, direction, height, vspeed, nspeed_length, tail):
     else:
         t = "-" + str(abs(height))
     if vspeed > 0:
-        t = t + '\u02c5'
+        t = t + '\u2b08'
     if vspeed < 0:
-        t = t + '\u02c4'
+        t = t + '\u2b09'
     tsize = draw.textsize(t, largefont)
     if tsize[0] + x + 4 * AIRCRAFT_SIZE - 2 > sizex:
         # would draw text outside, move to the left
