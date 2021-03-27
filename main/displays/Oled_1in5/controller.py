26867781bc00299a22e9aa70c3271f03860e09d2
TomBric <47101971+TomBric@users.noreply.github.com>
2021-03-27 11:43:19 +0100

    First steps Compass

--- main/displays/Oled_1in5/controller.py
+++ main/displays/Oled_1in5/controller.py
@@ -310,7 +310,7 @@ def compass(draw, heading, error_message):
     draw.ellipse((0, 0, sizex-1, sizey-1), outline="white", fill="black", width=2)
     image.paste(compass_aircraft, (round(zerox) - 30, 30))
     draw.line((zerox, 0, zerox, 30), fill="white", width=1)
-    draw.polygon((zerox, 12, zerox - 6, 0, zerox + 6, 0), fill="white")
+    # draw.polygon((zerox, 12, zerox - 6, 0, zerox + 6, 0), fill="white")
     for m in range(0, 360, 10):
         s = math.sin(math.radians(m - heading+ 90))
         c = math.cos(math.radians(m - heading + 90))
