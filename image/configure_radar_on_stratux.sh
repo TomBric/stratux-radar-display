2934ad45deffa95b28ba862aaad1b9041adca7d7
TomBric <47101971+TomBric@users.noreply.github.com>
2021-04-04 09:12:29 +0200

    Configuration skript for Stratux

--- image/configure_radar_on_stratux.sh
+++ image/configure_radar_on_stratux.sh
@@ -3,21 +3,11 @@
 # script configures basic libraries necessary for stratux-radar
 # script to be run as pi on stratux (without a zero pi)
 
-#enable usermod
-# sudo usermod -a -G spi,gpio,i2c pi
-
-# remove desktop packages
-# sudo apt purge xserver* lightdm* vlc* lxde* chromium* desktop* gnome* gstreamer* gtk* hicolor-icon-theme* lx* mesa* -y
-# sudo apt-get autoremove -y
-
 # luma files and more
 sudo apt-get update -y
-# sudo apt-get upgrade -y
 sudo apt install python3-pip python3-pil -y
 sudo apt-get install libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 -y
 sudo pip3 install luma.oled
-# sudo apt-get install git -y
-
 
 #websockets for radar
 sudo pip3 install websockets
@@ -32,9 +22,7 @@ sudo apt-get install libbluetooth-dev -y
 sudo pip3 install pybluez
 sudo pip3 install pydbus
 sudo pip3 install --upgrade PILLOW
-# sudo apt purge piwiz -y
-# necessary to disable bluetoothmessage "To turn on ..."
-# pip3 uninstall numpy  # remove previously installed version
+
 sudo apt install python3-numpy -y
 
 # get files from repo
@@ -44,28 +32,6 @@ sudo apt install python3-numpy -y
 echo "@reboot /bin/bash /home/pi/stratux-radar-display/image/stratux_radar.sh" | crontab -
 # only works if crontab is empty, otherwise use
 # crontab -l | sed "\$a@reboot /bin/bash /home/pi/stratux-radar-display/image/start_radar" | crontab -
-
-
-# bluetooth configuration
-# Enable a system wide pulseaudio server, otherwise audio in non-login sessions is not working
-#
-# configs in /etc/pulse/system.pa
-# sudo sed -i '$ a load-module module-bluetooth-discover' /etc/pulse/system.pa
-# sudo sed -i '$ a load-module module-bluetooth-policy' /etc/pulse/system.pa
-# sudo sed -i '$ a load-module module-switch-on-connect' /etc/pulse/system.pa
-
-# configs in /etc/pulse/client.conf to disable client spawns
-# sudo sed -i '$ a default-server = /var/run/pulse/native' /etc/pulse/client.conf
-# sudo sed -i '$ a autospawn = no' /etc/pulse/client.conf
-
-# allow user pulse bluetooth access
-# sudo addgroup pulse bluetooth
-# sudo addgroup pi pulse-access
-
-# start pulseaudio system wide
-# sudo cp /home/pi/stratux-radar-display/image/pulseaudio.service /etc/systemd/system/
-# sudo systemctl --system enable pulseaudio.service
-# sudo systemctl --system start pulseaudio.service
 
 # enable spi
 sudo raspi-config nonint do_spi 0
