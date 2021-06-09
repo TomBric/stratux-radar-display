13c11f50e4f3b9694e5bba38be13cad8246d794c
TomBric <47101971+TomBric@users.noreply.github.com>
2021-06-09 18:41:38 +0200

    Optical improvement status page

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -643,7 +643,9 @@ def bar(draw, y, text, val, max_val, yellow, red, unit="", valtext=None, minval 
         xval = bar_start + (bar_end - bar_start) * val / max_val
     else:
         xval = bar_start
-    draw.rectangle([bar_start, y, xval, y+VERYSMALL], fill=color, outline=None)
+    for b in range(bar_start, xval, 4):
+        draw.line([(b, y), (b, y-VERYSMALL)], fill="black",width=2)
+    # draw.rectangle([bar_start, y, xval, y+VERYSMALL], fill=color, outline=None)
     if valtext != None:
         t = valtext
     else:
