2c3ed326499ca5f483019f550c91f466870ed6c1
TomBric <47101971+TomBric@users.noreply.github.com>
2021-06-09 08:51:59 +0200

    Optical improvement status page

--- main/displays/Oled_1in5/controller.py
+++ main/displays/Oled_1in5/controller.py
@@ -575,7 +575,7 @@ def stratux(draw, stat, altitude, gps_alt):
     starty = bar(draw, starty, "1090", stat['ES_messages_last_minute'], stat['ES_messages_max'], 0, 0)
     if stat['OGN_connected']:
         starty = bar(draw, starty, "OGN", stat['OGN_messages_last_minute'], stat['OGN_messages_max'], 0, 0)
-        noise_text = str(stat['OGN_noise_db']) + "@" + str(stat['OGN_gain_db']) + " dB"
+        noise_text = str(round(stat['OGN_noise_db'],1)) + "@" + str(round(stat['OGN_gain_db'],1)) + "dB"
         starty = bar(draw, starty, "noise", stat['OGN_noise_db'], 25, 12, 18, unit="dB", minval=1, valtext= noise_text)
     if stat['UATRadio_connected']:
         starty = bar(draw, starty, "UAT", stat['UAT_messages_last_minute'], stat['UAT_messages_max'], 0, 0)
@@ -607,7 +607,10 @@ def stratux(draw, stat, altitude, gps_alt):
 
     fl = '{:3.0f}'.format(round(altitude) / 100)
     x = round_text(draw, 3, starty, "FL" + fl, "DarkBlue")
-    alt = '{:5.0f}'.format(gps_alt)
+    if stat['GPS_position_accuracy'] < 19999:
+        alt = '{:5.0f}'.format(gps_alt)
+    else:
+        alt="---"
     x = round_text(draw, x, starty, "Alt"+alt+"ft", "DimGray")
     if stat['IMUConnected']:
         col = "green"
