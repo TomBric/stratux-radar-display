542bad57da38b9212243238796c6ed7dbd8f572d
TomBric <47101971+TomBric@users.noreply.github.com>
2021-12-25 18:19:48 +0100

    Oled gmeter with graphics

--- main/displays/Oled_1in5/controller.py
+++ main/displays/Oled_1in5/controller.py
@@ -319,17 +319,17 @@ def gmeter(draw, current, maxg, ming, error_message):
     draw.line((zerox-(csize-msize-3)*c, zeroy-(csize-msize-3)*s, zerox+10*c, zeroy+10*s), fill="white", width=3)
 
     draw.text((zerox+10, 0), "G-Meter", font=smallfont, fill="yellow")
-    draw.text((zerox+5, 30), "max", font=smallfont, fill="cyan")
-    right_text(draw, 30, "{:+1.2f}".format(maxg), smallfont, fill="magenta")
+    draw.text((zerox+5, 35), "max", font=smallfont, fill="cyan")
+    right_text(draw, 35, "{:+1.2f}".format(maxg), smallfont, fill="magenta")
     if error_message:
         centered_text(draw, 57, error_message, largefont, fill="red")
-    draw.text((zerox+5, 80), "min", font=smallfont, fill="cyan")
-    right_text(draw, 80, "{:+1.2f}".format(ming), smallfont, fill="magenta")
+    draw.text((zerox+5, 75), "min", font=smallfont, fill="cyan")
+    right_text(draw, 75, "{:+1.2f}".format(ming), smallfont, fill="magenta")
 
     right = "Reset"
     textsize = draw.textsize(right, smallfont)
     draw.text((sizex - textsize[0], sizey - SMALL - 3), right, font=smallfont, fill="green", align="right")
-    middle = "  Mode"
+    middle = ""
     centered_text(draw, sizey - SMALL - 3, middle, smallfont, fill="green")
 
 
