290d7932f6dadb82810606ad8e08b39171f24081
TomBric <47101971+TomBric@users.noreply.github.com>
2022-01-05 22:20:30 +0100

    Introducing flight time calculations

--- main/flighttime.py
+++ main/flighttime.py
@@ -31,7 +31,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from datetime import datetime, timezone, timedelta
+import datetime
 import logging
 import json
 import statusui
