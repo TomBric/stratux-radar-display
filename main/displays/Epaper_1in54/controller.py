1cdaf8086dcc964dbeb7bd3f0efcb07708a85ce8
TomBric <47101971+TomBric@users.noreply.github.com>
2022-01-25 17:28:39 +0100

    Starting setup Epaper-Display 1.54 inch

--- main/displays/Epaper_1in54/controller.py
+++ main/displays/Epaper_1in54/controller.py
@@ -320,7 +320,7 @@ def situation(draw, connected, gpsconnected, ownalt, course, range, altdifferenc
         draw.text((sizex - textsize[0] - 5, sizey - SMALL), t, font=awesomefont, fill="black")
 
     # optical keep alive bar at right side
-    draw.line((sizex-6, 110+optical_bar*5, sizex-6, 80+optical_bar*5+6), fill="black", width=4)
+    draw.line((sizex-6, 110+optical_bar*5, sizex-6, 110+optical_bar*5+6), fill="black", width=4)
 
 
 def timer(draw, utctime, stoptime, laptime, laptime_head, left_text, middle_text, right_t, timer_runs):
