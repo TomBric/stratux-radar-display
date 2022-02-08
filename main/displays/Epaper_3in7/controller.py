44eec4df96e9735eddb59e195c4e791b152a7706
TomBric <47101971+TomBric@users.noreply.github.com>
2022-02-08 19:01:05 +0100

    Starting co-warner integration

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -855,7 +855,7 @@ def cowarner(draw, co_values, co_max, r0, timeout):   # draw graph and co values
         right_text(draw, 60, "{:3d}".format(co_values[len(co_values) - 1]), smallfont, fill="black")
     draw.text((320, 90+SMALL-VERYSMALL), "CO Max:", font=verysmallfont, fill="black")
     right_text(draw, 90, "{:3d}".format(co_max), smallfont, fill="black")
-    draw.text((320, 180+SMALL-VERYSMALL), "R0: [Ohms]", font=verysmallfont, fill="black")
+    draw.text((320, 180+SMALL-VERYSMALL), "R0 [Ohms]:", font=verysmallfont, fill="black")
     right_text(draw, 180, "{:5.2f}".format(r0), smallfont, fill="black")
 
     draw.text((8, sizey - SMALL - 3), "Calibrate", font=smallfont, fill="black", align="left")
