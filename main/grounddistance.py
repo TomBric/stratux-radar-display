2c673887799688595edb8c0b78e2cc2d2e632fc6
TomBric <47101971+TomBric@users.noreply.github.com>
2022-11-16 15:21:42 +0100

    Simulation mode to test grounddistance added

--- main/grounddistance.py
+++ main/grounddistance.py
@@ -324,4 +324,4 @@ def read_simulation_data():    # return False, 0, 0, 0 with error, else True, gr
         return False, 0, 0, 0
     rlog.debug("GroundDistance: Simulation data read from " + SIM_DATA_FILE + ": " +
                json.dumps(sim_data, sort_keys=True, indent=4))
-    return True, sim_data['distance'], sim_data['speed'], sim_data['altitude']
+    return True, sim_data['g_distance'], sim_data['gps_speed'], sim_data['own_altitude']
