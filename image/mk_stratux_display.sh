91ab38f08ac9236c2fd7bd51d3e1302e04ef0180
TomBric <47101971+TomBric@users.noreply.github.com>
2023-06-13 17:36:06 +0200

    base image changed to RaspiOS 2023-05-03 and xz as compression method

--- image/mk_stratux_display.sh
+++ image/mk_stratux_display.sh
@@ -45,7 +45,7 @@ cd $TMPDIR || die "cd failed"
 
 # Download/extract image
 wget -c $BASE_IMAGE_URL || die "Download failed"
-xz $ZIPNAME || die "Extracting image failed"
+xz -d $ZIPNAME || die "Extracting image failed"
 
 
 # Check where in the image the root partition begins:
