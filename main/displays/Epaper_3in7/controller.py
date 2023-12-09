21964a697b5bdc6114cb5785016dd649d505d5d6
TomBric <47101971+TomBric@users.noreply.github.com>
2023-12-09 15:44:19 +0100

    Changed textsize to textbbox or textlength due to removal of textsize in pillow10

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -824,14 +824,14 @@ def graph(draw, xpos, ypos, xsize, ysize, data, minvalue, maxvalue, value_line1,
         x = x + offset
     lastpoint = None
     for i in range(0, len(data)):
-        y = ypos-3 + ysize - ysize * (data[i] - minvalue) / (maxvalue - minvalue)
+        y = ypos-4 + ysize - ysize * (data[i] - minvalue) / (maxvalue - minvalue)
         if y < ypos:
             y = ypos   # if value is outside
         if y > ypos+ysize-1:
             x = ypos+ysize-1
         if i >= 1:  # we need at least two points before we draw
             x = xpos + i * xsize / (len(data)-1)
-            draw.line([lastpoint, (x, y)], fill="black", width=2)
+            draw.line([lastpoint, (x, y)], fill="black", width=3)
         else:
             x = xpos
         lastpoint = (x, y)
