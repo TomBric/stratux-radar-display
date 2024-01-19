c4c05bad7ea2b3266b20640367961132c2265f5b
TomBric <47101971+TomBric@users.noreply.github.com>
2024-01-19 18:00:38 +0100

    included checklists, first version test, for 3.7 only

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -1081,7 +1081,8 @@ def checklist(draw, checklist_name, checklist_items, current_index):
     size = checklist_y['from']
     while True:
         for item in range(top_index, current_index + 2):   # check if also next element fits on screen
-            size = checklist_topic(draw, size, checklist_items[item], highlighted=False, toprint=False)
+            if item < len(checklist_items):
+                size = checklist_topic(draw, size, checklist_items[item], highlighted=False, toprint=False)
         if size <= checklist_y['to']:  # fits in screen, no reason to scroll
             break
         else:
