4943d7e799c56c575a65b67e0cdce9800018044f
TomBric <47101971+TomBric@users.noreply.github.com>
2024-02-06 08:04:37 +0100

    Changes for Epaper_1in54

--- main/displays/Epaper_1in54/controller.py
+++ main/displays/Epaper_1in54/controller.py
@@ -674,8 +674,8 @@ def stratux(draw, stat, altitude, gps_alt, gps_quality):
     right_text(draw, starty, gps, verysmallfont, "black")
     starty += VERYSMALL+2
 
-    draw.text(0, starty, "P-Alt {0:.0f}ft".format(altitude), fill="black")
-    round_text(draw, x, starty, "Corr {0:+}ft".format(stat['AltitudeOffset']), fill="black")
+    draw.text((0, starty), "P-Alt {0:.0f}ft".format(altitude), font=verysmallfont, fill="black")
+    right_text(draw, starty, "Corr {0:+}ft".format(stat['AltitudeOffset']), verysmallfont, "black")
     starty += VERYSMALL + 2
     x = round_text(draw, 0, starty, "IMU", "white", stat['IMUConnected'], out="black")
     round_text(draw, x+10, starty, "BMP", "white", stat['BMPConnected'], out="black")
