b5e42c27da08b3e360f01d8c81cfa8f9a9a7c452
TomBric <47101971+TomBric@users.noreply.github.com>
2024-02-06 08:18:04 +0100

    Changes for Epaper_1in54

--- main/displays/Epaper_1in54/controller.py
+++ main/displays/Epaper_1in54/controller.py
@@ -676,7 +676,7 @@ def stratux(draw, stat, altitude, gps_alt, gps_quality):
 
     draw.text((0, starty), "P-Alt {0:.0f}ft".format(altitude), font=verysmallfont, fill="black")
     right_text(draw, starty, "Corr {0:+}ft".format(stat['AltitudeOffset']), verysmallfont, "black")
-    starty += VERYSMALL + 4
+    starty += VERYSMALL + 6
     x = round_text(draw, 0, starty, "IMU", "white", stat['IMUConnected'], out="black")
     round_text(draw, x+10, starty, "BMP", "white", stat['BMPConnected'], out="black")
     if stat['GPS_position_accuracy'] < 19999:
