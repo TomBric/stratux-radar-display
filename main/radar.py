8d83b5c7338aad32b239d2064e47fe9dd7430ec2
TomBric <47101971+TomBric@users.noreply.github.com>
2024-02-07 08:18:47 +0100

    Removing TaskGroup (from python 3.11 on only)

--- main/radar.py
+++ main/radar.py
@@ -778,12 +778,12 @@ async def display_and_cutoff():
 
 
 async def coroutines():
-    tr_handler = tg.create_task(listen_forever(url_radar_ws, "TrafficHandler", new_traffic, rlog))
-    sit_handler = tg.create_task(listen_forever(url_situation_ws, "SituationHandler", new_situation, rlog))
-    dis_cutoff = tg.create_task(display_and_cutoff())
-    sensor_reader = tg.create_task(cowarner.read_sensors())
-    ground_sensor_reader = tg.create_task(grounddistance.read_ground_sensor())
-    u_interface = tg.create_task(user_interface())
+    tr_handler = asyncio.create_task(listen_forever(url_radar_ws, "TrafficHandler", new_traffic, rlog))
+    sit_handler = asyncio.create_task(listen_forever(url_situation_ws, "SituationHandler", new_situation, rlog))
+    dis_cutoff = asyncio.create_task(display_and_cutoff())
+    sensor_reader = asyncio.create_task(cowarner.read_sensors())
+    ground_sensor_reader = asyncio.create_task(grounddistance.read_ground_sensor())
+    u_interface = asyncio.create_task(user_interface())
     await asyncio.gather(tr_handler, sit_handler, dis_cutoff, u_interface, sensor_reader, ground_sensor_reader)
     # With python 3.11 a TaskGroup could be used to ensure theat coroutine exceptions are propagated to main task
 
