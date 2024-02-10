59cd6d730757e61c83894b8193aadc5df8ced495
TomBric <47101971+TomBric@users.noreply.github.com>
2024-02-10 17:00:00 +0100

    Base image changed to bookworm

--- image/mk_configure_radar.sh
+++ image/mk_configure_radar.sh
@@ -15,10 +15,10 @@ fi
 
 # try to reduce writing to SD card as much as possible, so they don't get bricked when yanking the power cable
 # Disable swap...
-# systemctl disable dphys-swapfile
-# apt purge -y dphys-swapfile
-# apt autoremove -y
-# apt clean
+systemctl disable dphys-swapfile
+apt purge -y dphys-swapfile
+apt autoremove -y
+apt clean
 
 # enable ssh
 raspi-config nonint do_ssh 0
@@ -27,7 +27,7 @@ raspi-config nonint do_ssh 0
 # chpasswd pi:raspberry
 # usermod -aG sudo pi
 # set wifi with raspi-config
-raspi-config nonint do_wifi_ssid_passphrase stratux
+# raspi-config nonint do_wifi_ssid_passphrase stratux
 
 # enable spi and i2c (for cowarner)
 raspi-config nonint do_spi 0
@@ -35,15 +35,15 @@ raspi-config nonint do_i2c 0
 
 # for groundsensor, disable ssh over serial cause it is needed for the sensor
 # disable ssh over serial otherwise
-sed -i /boot/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
-sed -i /boot/cmdline.txt -e "s/console=serial0,[0-9]\+ //"
-sed -i /boot/cmdline.txt -e "s/console=tty[0-9]\+ //"
+sed -i /boot/firmware/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
+sed -i /boot/firmware/cmdline.txt -e "s/console=serial0,[0-9]\+ //"
+sed -i /boot/firmware/cmdline.txt -e "s/console=tty[0-9]\+ //"
 # modify /boot/config.text for groundsensor
 {
   echo "# modification for ultrasonic ground sensor"
   echo "enable_uart=1"
   echo "dtoverlay=miniuart-bt"
-} | tee -a /boot/config.txt
+} | tee -a /boot/firmware/config.txt
 
 
 apt install python3-pip python3-numpy python3-websockets python3-serial python3-luma.oled python3-xmltodict python3-pydbus -y
