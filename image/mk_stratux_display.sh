d5e66c45a9647fa51fa1deecee4d24429ca2c776
TomBric <47101971+TomBric@users.noreply.github.com>
2024-03-19 08:29:22 +0100

    mk_stratux_display update, Imagesize increased to 6G

--- image/mk_stratux_display.sh
+++ image/mk_stratux_display.sh
@@ -101,7 +101,7 @@ bootoffset=$(( 512*bootoffset ))
 
 # Original image partition is too small to hold our stuff.. resize it to 5120 Mb
 # Append one GB and truncate to size
-truncate -s 5120M $IMGNAME || die "Image resize failed"
+truncate -s 6144M $IMGNAME || die "Image resize failed"
 lo=$(losetup -f)
 losetup "$lo" $IMGNAME
 partprobe "$lo"
