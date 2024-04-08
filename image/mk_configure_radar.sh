ad1784fa2f1db98cc60bc95d1803f300cc1d9488
TomBric <47101971+TomBric@users.noreply.github.com>
2024-04-08 08:19:49 +0200

    changed locations of cmdline.txt and config.txt due to bookworm

--- image/mk_configure_radar.sh
+++ image/mk_configure_radar.sh
@@ -24,15 +24,15 @@ raspi-config nonint do_i2c 0
 
 # for groundsensor, disable ssh over serial cause it is needed for the sensor
 # disable ssh over serial otherwise
-sed -i /boot/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
-sed -i /boot/cmdline.txt -e "s/console=serial0,[0-9]\+ //"
-sed -i /boot/cmdline.txt -e "s/console=tty[0-9]\+ //"
-# modify /boot/config.text for groundsensor
+sed -i /boot/firmware/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
+sed -i /boot/firmware/cmdline.txt -e "s/console=serial0,[0-9]\+ //"
+sed -i /boot/firmware/cmdline.txt -e "s/console=tty[0-9]\+ //"
+# modify /boot/firmware/config.text for groundsensor
 {
   echo "# modification for ultrasonic ground sensor"
   echo "enable_uart=1"
   echo "dtoverlay=miniuart-bt"
-} | tee -a /boot/config.txt
+} | tee -a /boot/firmware/config.txt
 
 # sound and espeak
 apt install libasound2-dev libasound2-doc python3-alsaaudio espeak-ng espeak-ng-data -y
