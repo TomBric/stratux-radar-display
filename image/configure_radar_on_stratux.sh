4209715fc9e9f6d6306c8e3b7c58d55697f1a6da
TomBric <47101971+TomBric@users.noreply.github.com>
2024-04-11 13:30:31 +0200

    configure radar for stratux-eu030

--- image/configure_radar_on_stratux.sh
+++ image/configure_radar_on_stratux.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # script configures basic libraries necessary for stratux-radar
-# script to be run as pi on stratux (without a zero pi)
+# script to be run as root on stratux (without a zero pi)
 
 set -x
 # luma files, pip3 and more
-sudo apt-get update -y
-sudo apt install libasound2-dev libasound2-doc python3-alsaaudio espeak-ng espeak-ng-data -y
-sudo apt install python3-websockets python3-xmltodict python3-pydbus python3-luma.oled python3-numpy -y
-sudo pip3 install pybluez py-espeak-ng ADS1x15-ADC --break-system-packages
+apt-get update -y
+apt install libasound2-dev libasound2-doc python3-alsaaudio espeak-ng espeak-ng-data -y
+apt install python3-websockets python3-xmltodict python3-pydbus python3-luma.oled python3-numpy -y
+pip3 install pybluez py-espeak-ng ADS1x15-ADC --break-system-packages
 
 # copy simple checklist once, can be changed later
-cp /home/pi/stratux-radar-display/config/checklist.example_small.xml /home/pi/stratux-radar-display/config/checklist.xml
+sudo -u pi cp /home/pi/stratux-radar-display/config/checklist.example_small.xml /home/pi/stratux-radar-display/config/checklist.xml
 
 # disable bluetooth in any case, it is not working directly on Stratux
-sed -i 's/-b/ /g' /home/pi/stratux-radar-display/image/stratux_radar.sh
+sudo -u pi sed -i 's/-b/ /g' /home/pi/stratux-radar-display/image/stratux_radar.sh
 # include autostart into crontab, so that radar starts on every boot
-echo "@reboot /bin/bash /home/pi/stratux-radar-display/image/stratux_radar.sh" | crontab -
+echo "@reboot /bin/bash /home/pi/stratux-radar-display/image/stratux_radar.sh" | crontab -u pi -
 # only works if crontab is empty, otherwise use
 # crontab -l | sed "\$a@reboot /bin/bash /home/pi/stratux-radar-display/image/start_radar" | crontab -
 
 # enable spi
-sudo raspi-config nonint do_spi 0
+raspi-config nonint do_spi 0
