2cb79a51016031819fae10230125e05b3b30aaff
TomBric <47101971+TomBric@users.noreply.github.com>
2024-04-20 14:49:56 +0200

    Introducing speech warnings for landing using multithreading

--- main/grounddistance.py
+++ main/grounddistance.py
@@ -278,9 +278,6 @@ def calc_distance_speaker(stat):
     else:
         ground_distance = 0.0
     if indicate_distance and fly_status == 1:
-        print("gps-levels reached :{0}".format(gps_upper))
-        print("ground-levels reached :{0}".format(sensor_upper))
-        print('GPS-distance {0} ground_distance {1}'.format(gps_distance, ground_distance))
         for (i, height) in enumerate(gps_warnings):
             if gps_distance < height and gps_upper[i]:
                 # distance is reached and was before higher than hysteresis
