46dc1e3e6c505c43f586da1dfee3a9c2208b644f
TomBric <47101971+TomBric@users.noreply.github.com>
2024-04-27 12:35:46 +0200

    Configure radar on stratux for eu030 with pipewire

--- image/mk_stratux_display.sh
+++ image/mk_stratux_display.sh
@@ -129,7 +129,7 @@ chroot mnt apt install git -y
 cd mnt/$DISPLAY_SRC || die "cd failed"
 sudo -u pi git clone --recursive -b "$BRANCH" https://github.com/TomBric/stratux-radar-display.git
 cd ../../../
-chroot --userspec=pi mnt sudo /bin/bash $DISPLAY_SRC/stratux-radar-display/image/mk_configure_radar.sh "$BRANCH"
+chroot mnt sudo /bin/bash $DISPLAY_SRC/stratux-radar-display/image/mk_configure_radar.sh "$BRANCH"
 
 # mkdir -p out
 umount mnt/boot
