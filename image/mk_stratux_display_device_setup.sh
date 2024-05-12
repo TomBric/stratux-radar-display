14d206b98778582ac024c39628339e35e07f850f
TomBric <47101971+TomBric@users.noreply.github.com>
2024-05-12 14:25:15 +0200

    Modifying make files to create working startup

--- image/mk_stratux_display_device_setup.sh
+++ image/mk_stratux_display_device_setup.sh
@@ -5,12 +5,12 @@
 # Do NOT run directly. This is called from mk_stratux_display.sh via chroot
 # This is used to trigger systemctl commands inside a chrooted environmen
 
-# set -x
-mount -t proc proc /proc
+set -x
 # enable ssh
 raspi-config nonint do_ssh 0
 # enable spi and i2c (for cowarner)
 raspi-config nonint do_spi 0
 raspi-config nonint do_i2c 0
 raspi-config nonint do_boot_behaviour B2
+loginctl enable-linger pi
 # enable autologin
