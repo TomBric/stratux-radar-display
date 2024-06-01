76b610c12c22e2275a31af547d7595859398543a
TomBric <47101971+TomBric@users.noreply.github.com>
2024-06-01 18:47:51 +0200

    Change base image of stratux to eu031

--- image/configure_radar_on_stratux.sh
+++ image/configure_radar_on_stratux.sh
@@ -8,7 +8,7 @@ apt install git python3-pip -y
 # no sound or bluetooth on stratux
 # apt install pipewire pipewire-audio pipewire-alsa libspa-0.2-bluetooth libttspico-utils
 apt install python3-alsaaudio -y
-apt install python3-websockets python3-xmltodict python3-pydbus python3-luma.oled python3-pip python3-numpy -y
+apt install python3-websockets python3-xmltodict python3-pydbus python3-luma.oled python3-pip python3-numpy python3-pygame -y
 su pi -c "pip3 install  ADS1x15-ADC --break-system-packages"
 
 # copy simple checklist once, can be changed later
