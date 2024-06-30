5fdb9ac2fe45b207ce15ba6baa3cefd42b44f16e
TomBric <47101971+TomBric@users.noreply.github.com>
2024-06-30 15:03:02 +0200

    Changes in radarbluez for sound output in stratux

--- image/configure_radar_on_stratux.sh
+++ image/configure_radar_on_stratux.sh
@@ -11,6 +11,22 @@ apt install pipewire pipewire-audio pipewire-alsa libspa-0.2-bluetooth python3-a
 apt install python3-websockets python3-xmltodict python3-pydbus python3-luma.oled python3-pip python3-numpy python3-pygame -y
 su pi -c "pip3 install  ADS1x15-ADC --break-system-packages"
 
+# check to get sound running on stratux
+# this is the same effect as loginctl enable-linger pi
+mkdir -p /var/lib/systemd/linger
+touch /var/lib/systemd/linger/pi
+
+# install and start service to start radar
+su pi -c "mkdir -p /home/pi/.config/systemd/user/"
+su pi -c "cp /home/pi/stratux-radar-display/image/systemctl-autostart-radar.service /home/pi/.config/systemd/user/autostart-radar.service"
+# create a symlink, do do the same as: systemctl --user -M pi@ enable autostart-radar
+su pi -c "mkdir /home/pi/.config/systemd/user/default.target.wants"
+su pi -c "ln -s /home/pi/.config/systemd/user/autostart-radar.service /home/pi/.config/systemd/user/default.target.wants/autostart-radar.service"
+
+# change log level of rtkit, otherwise this fills journal with tons of useless info
+sed -i '/\[Service\]/a LogLevelMax=notice' /usr/lib/systemd/system/rtkit-daemon.service
+# ---------------
+
 # copy simple checklist once, can be changed later
 su pi -c "cp /home/pi/stratux-radar-display/config/checklist.example_small.xml /home/pi/stratux-radar-display/config/checklist.xml"
 
