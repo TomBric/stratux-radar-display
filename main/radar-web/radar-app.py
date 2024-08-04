990b98402d0bdc53ce2b1447f0c87a3c25db9dc5
TomBric <47101971+TomBric@users.noreply.github.com>
2024-08-04 20:40:26 +0200

    First version with configuration web server in flask

--- main/radar-web/radar-app.py
+++ main/radar-web/radar-app.py
@@ -40,7 +40,7 @@ import threading
 from flask import Flask, render_template, request, flash, redirect, url_for
 from markupsafe import Markup
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms.validators import DataRequired, Length, Regexp
+from wtforms.validators import DataRequired, Length, Regexp, IPAddress
 from wtforms.fields import *
 from flask_bootstrap import Bootstrap5, SwitchField
 
