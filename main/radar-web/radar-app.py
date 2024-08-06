05f329bbb805a3965be57afa24a7f70cd35d6957
TomBric <47101971+TomBric@users.noreply.github.com>
2024-08-06 08:36:54 +0200

    First version with configuration web server in flask

--- main/radar-web/radar-app.py
+++ main/radar-web/radar-app.py
@@ -37,6 +37,8 @@ import os
 import signal
 import threading
 # add parent path to syspath
+import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import arguments
 import radarmodes
