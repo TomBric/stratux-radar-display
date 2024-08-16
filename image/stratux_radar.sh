0d4c5831aaf7da6d90c46ba5ab6112878c18f40d
TomBric <47101971+TomBric@users.noreply.github.com>
2024-08-16 08:02:26 +0200

    Implementing radar-app actions

--- image/stratux_radar.sh
+++ image/stratux_radar.sh
@@ -3,7 +3,7 @@
 # This is called as pi user via autostart-radar.service or on stratux via crontab on every boot
 
 # start configuration webserver
-cd /home/pi/stratux-radar-display/radar-web && python3 radar-app.py 3 &
+cd /home/pi/stratux-radar-display/radar-web && python3 radar-app.py -t 3 &
 
 # start radar. This line will be configured via the configuration webserver
 cd /home/pi/stratux-radar-display/main && python3 radar.py -r -d Epaper3in7 -c 192.168.10.1
