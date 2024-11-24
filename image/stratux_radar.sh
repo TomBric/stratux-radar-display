f62c0b9f0dbed745cbfe8b851742d6914c79e885
TomBric <47101971+TomBric@users.noreply.github.com>
2024-11-24 12:59:51 +0100

    epdconfig refresh

--- image/stratux_radar.sh
+++ image/stratux_radar.sh
@@ -3,7 +3,7 @@
 # This is called as pi user via autostart-radar.service or on stratux via crontab on every boot
 
 # start configuration webserver
-cd /home/pi/stratux-radar-display/main/radar-web && python3 radarapp.py -t 3 &
+cd /home/pi/stratux-radar-display/main/radar-web && python3 radarapp.py -t 10 &
 
 # start radar. This line will be configured via the configuration webserver
 cd /home/pi/stratux-radar-display/main && python3 radar.py -r -d Epaper_3in7 -c 192.168.10.1
