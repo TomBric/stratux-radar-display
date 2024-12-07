5c02010ebbcfe5663b765727320f0417bb759223
TomBric <47101971+TomBric@users.noreply.github.com>
2024-12-07 17:37:43 +0100

    Configure for port 81 on stratux, 80 if on Zero

--- image/mk_config_webapp.sh
+++ image/mk_config_webapp.sh
@@ -3,9 +3,30 @@
 # script configures libraries and settings necessary for stratux-radar web configuration
 # script to be run as root
 # called via configure_radar as sudo with target image mounted
+# run with option -s to configure webapp on stratux (uses port 81 instead of 80)
 # example: /bin/bash mk_config_webapp.sh
 
 set -x
+
+ON_STRATUX = false
+
+while getopts ":s" opt; do
+  case $opt in
+    s)
+      ON_STRATUX=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      exit 1
+      ;;
+    :)
+      echo "option -$OPTARG requires a value."
+      exit 1
+      ;;
+  esac
+done
+
+
 apt update
 # apt upgrade -y
 apt install nginx -y
@@ -14,6 +35,10 @@ pip3 install bootstrap-flask flask-wtf werkzeug --break-system-packages
 # generate reverse proxy config
 unlink /etc/nginx/sites-enabled/default
 cp radar_reverse.conf /etc/nginx/sites-available/
+if [ "ON_STRATUX" = true ]; then
+  # change port 80 to 81 for nginx, since stratux already offers web on 80
+  sed -i 's/listen 80/listen 81/g' /etc/nginx/sites-available/
+fi
 ln -s /etc/nginx/sites-available/radar_reverse.conf /etc/nginx/sites-enabled/
 # restart to read new configuration
 systemctl restart nginx
