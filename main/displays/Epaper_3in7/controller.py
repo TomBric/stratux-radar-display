411ca34f0e4a822b2e206da49825053e7427b557
TomBric <47101971+TomBric@users.noreply.github.com>
2025-01-29 07:52:31 +0100

    Refactoring flighttime

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -329,11 +329,12 @@ class Epaper3in7(dcommon.GenericDisplay):
     def round_text(self, x, y, text, bg_color=None, yesno=True, out_color=None):
         # bg color is color of background, if none given, this is the normal background for this display
         # out_color is coler of outline, if none given, outline is not
+        # if yesno is false, the text is crossed out
         bg_color = bg_color or self.BG_COLOR
         tl = self.draw.textlength(text, self.smallfont)
         self.draw.rounded_rectangle([x, y, x + tl + 10, y + self.VERYSMALL + 2], radius=4, fill=bg_color)
         if out_color is not None:
-            self.draw.rounded_rectangle([x, y, x + tl + 10, y + self.VERYSMALL + 2], radius=4, outline=out_color)
+            self.draw.rounded_rectangle([x, y, x + tl, y + self.VERYSMALL], radius=4, outline=out_color)
         self.draw.text((x+5, y), text, font=self.verysmallfont, fill= self.TEXT_COLOR)
         if not yesno:
             self.draw.line([x, y+self.VERYSMALL+2, x+tl+10, y], fill= self.TEXT_COLOR, width=2)
