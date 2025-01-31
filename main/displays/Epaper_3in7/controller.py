140e4b80b1b8f5cb8ae9580ca8b2453dbcc3c315
TomBric <47101971+TomBric@users.noreply.github.com>
2025-01-31 14:03:39 +0100

    Refactoring flighttime

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -230,15 +230,15 @@ class Epaper3in7(dcommon.GenericDisplay):
             ("max", f"{vertical_max:+1.0f}"),
             ("min", f"{vertical_min:+1.0f}")
         )
-        self.dashboard(self.sizey + self.SMALL, self.SMALL,
-                       self.sizex - self.sizey + 2*self.SMALL, lines, rounding=True, headline="Vert Speed [ft/min]",
+        self.dashboard(self.sizey + self.SMALL, self.VERYSMALL,
+                       self.sizex - self.sizey - 2 * self.SMALL, lines, rounding=True, headline="Vert Speed [ft/min]",
                        headline_size=self.SMALL)
         lines = (
             ("Flight-Level", f"{round(flight_level / 100):1.0f}"),
             ("GPS-Alt [ft]", f"{gps_altitude:1.0f}"),
             ("GpsSpd [kts]", f"{gps_speed:1.1f}")
         )
-        self.dashboard(self.sizey + self.SMALL, self.sizex // 2 + self.SMALL, self.sizex - self.sizey + 2 * self.SMALL,
+        self.dashboard(self.sizey + self.SMALL, self.sizey // 2 , self.sizex - self.sizey - 2 * self.SMALL,
                        lines, rounding=True)
         # right data display
         # self.draw.text((300, 10), "Vert Speed [ft/min]", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR, align="left")
