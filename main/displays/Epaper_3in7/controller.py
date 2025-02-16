f9a00cbcaed6e0d8ebd5e8fbbd30dd3d70a6443c
TomBric <47101971+TomBric@users.noreply.github.com>
2025-02-16 09:55:45 +0100

    CO display refactored

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -289,18 +289,13 @@ class Epaper3in7(dcommon.GenericDisplay):
             alarmstr = f"CO: {alarmppm} ppm longer {alarmperiod // 60} min" \
                 if alarmperiod > 60 else f"CO: {alarmppm} ppm longer {alarmperiod} sec"
             self.centered_text(0, alarmstr, self.LARGE)
-
-        self.graph(0, 40, 300, 200, co_values, 0, 120, 50, 100, timeout, self.TEXT_COLOR, self.TEXT_COLOR, self.TEXT_COLOR, self.BG_COLOR, 3, 3, 5, 3)
-        self.draw.text((320, 50 + self.SMALL - self.VERYSMALL), "Warnlevel:", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
-        self.right_text(50, f"{alarmlevel:3d}", self.SMALL, offset=5)
-
+        self.dashboard(320, 50, self.sizex - 320 - 5, [("Warnlevel:", f"{alarmlevel:3d}")])
         lines = [
             ("CO act:", f"{co_values[-1]:3d}") if co_values else ("CO act:", "---"),
             ("CO max:", f"{co_max:3d}")
         ]
         self.dashboard(320, 100, self.sizex - 320 - 5, lines)
         self.dashboard(320, 196, self.sizex - 320 - 5, [("R0", f"{r0 / 1000:.1f}k")])
-
         self.bottom_line("Calibrate", "Mode", "Reset")
 
     def distance(self, now, gps_valid, gps_quality, gps_h_accuracy, distance_valid, gps_distance, gps_speed, baro_valid,
