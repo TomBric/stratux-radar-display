9ef249f372a3e40339723b1f14c103b0aa258904
TomBric <47101971+TomBric@users.noreply.github.com>
2025-03-16 10:45:41 +0100

    scripts to build images updated

--- image/mk_radar_on_stratux.sh
+++ image/mk_radar_on_stratux.sh
@@ -70,24 +70,20 @@ wget -c "$BASE_IMAGE_URL" || die "Download failed"
 unzip "$ZIPNAME" || die "Extracting image failed"
 
 # Check where in the image the root partition begins:
-sector=$(fdisk -l "$IMGNAME" | grep Linux | awk -F ' ' '{print $2}')
-partoffset=$(( 512*sector ))
-bootoffset=$(fdisk -l "$IMGNAME" | grep W95 | awk -F ' ' '{print $2}')
-if [[ $bootoffset == "*" ]]; then
-    bootoffset=$(fdisk -l "$IMGNAME" | grep W95 | awk -F ' ' '{print $3}') # if boot flag is set...
-fi
-bootoffset=$(( 512*bootoffset ))
+bootoffset=$(parted $IMGNAME unit B p | grep fat32 | awk -F ' ' '{print $2}')
+bootoffset=${bootoffset::-1}
+partoffset=$(parted $IMGNAME unit B p | grep ext4 | awk -F ' ' '{print $2}')
+partoffset=${partoffset::-1}
 
-# Original image partition is too small to hold our stuff.. resize it to 5120 Mb
-# Append one GB and truncate to size
-truncate -s 6144M "$IMGNAME" || die "Image resize failed"
+# Original image partition is too small to hold our stuff.. resize it to 4gb
+truncate -s 4096M $IMGNAME || die "Image resize failed"
 lo=$(losetup -f)
-losetup "$lo" "$IMGNAME"
-partprobe "$lo"
-e2fsck -f "${lo}"p2
-parted "${lo}" resizepart 2 100%
-partprobe "$lo" || die "Partprobe failed failed"
-resize2fs -p "${lo}"p2 || die "FS resize failed"
+losetup $lo $IMGNAME
+partprobe $lo
+e2fsck -y -f ${lo}p2
+parted --script ${lo} resizepart 2 100%
+partprobe $lo || die "Partprobe failed failed"
+resize2fs -p ${lo}p2 || die "FS resize failed"
 
 # Mount image locally, clone our repo, install packages..
 mkdir -p mnt
@@ -116,20 +112,19 @@ chroot mnt /bin/bash $DISPLAY_SRC/stratux-radar-display/image/configure_radar_on
 umount mnt/boot
 umount mnt
 
+
 # Shrink the image to minimum size.. it's still larger than it really needs to be, but whatever
-minsize=$(resize2fs -P "${lo}"p2 | rev | cut -d' ' -f 1 | rev)
-minsizeBytes=$((minsize * 4096))
-e2fsck -f "${lo}"p2
-resize2fs -p "${lo}"p2 "$minsize"
-
-zerofree "${lo}"p2 # for smaller zip
-
-bytesEnd=$((partoffset + minsizeBytes))
-parted -s "${lo}" resizepart 2 ${bytesEnd}B yes
-partprobe "$lo"
-
-losetup -d "${lo}"
-truncate -s $((bytesEnd + 4096)) "$IMGNAME"
+minsize=$(resize2fs -P ${lo}p2 | rev | cut -d' ' -f 1 | rev)
+minsizeBytes=$(($minsize * 4096))
+e2fsck -f ${lo}p2
+resize2fs -p ${lo}p2 $minsize
+zerofree ${lo}p2 # for smaller zip
+bytesEnd=$(($partoffset + $minsizeBytes))
+losetup -d ${lo}
+# parted --script $IMGNAME resizepart 2 ${bytesEnd}B Yes doesn't seem tow rok any more... echo yes | parted .. neither. So we re-create partition with proper size
+parted --script $IMGNAME rm 2
+parted --script $IMGNAME unit B mkpart primary ext4 ${partoffset}B ${bytesEnd}B
+truncate -s $(($bytesEnd + 4096)) $IMGNAME
 
 
 cd "$SRCDIR" || die "cd failed"
