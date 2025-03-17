bd78413b86527f5ede9c784160ca983249a00ce0
TomBric <47101971+TomBric@users.noreply.github.com>
2025-03-17 15:25:23 +0100

    scripts to build images updated, removed unnecessary packages to minimize image size

--- image/mk_stratux_display.sh
+++ image/mk_stratux_display.sh
@@ -86,7 +86,7 @@ partoffset=$(parted $IMGNAME unit B p | grep ext4 | awk -F ' ' '{print $2}')
 partoffset=${partoffset::-1}
 
 # Original image partition is too small to hold our stuff.. resize it to 5gb
-truncate -s 5120 $IMGNAME || die "Image resize failed"
+truncate -s 5120M $IMGNAME || die "Image resize failed"
 lo=$(losetup -f)
 losetup $lo $IMGNAME
 partprobe $lo
