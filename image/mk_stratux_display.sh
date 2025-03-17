b4398a42370a9b43251401cef9611b3a1022c280
TomBric <47101971+TomBric@users.noreply.github.com>
2025-03-17 09:59:33 +0100

    scripts to build images updated, removed unnecessary packages to minimize image size

--- image/mk_stratux_display.sh
+++ image/mk_stratux_display.sh
@@ -77,7 +77,7 @@ cd $TMPDIR || die "cd failed"
 
 # Download/extract image
 wget -c $BASE_IMAGE_URL || die "Download failed"
-xz -d $ZIPNAME || die "Extracting image failed"
+unxz -k $ZIPNAME || die "Extracting image failed"
 
 # Check where in the image the root partition begins:
 bootoffset=$(parted $IMGNAME unit B p | grep fat32 | awk -F ' ' '{print $2}')
