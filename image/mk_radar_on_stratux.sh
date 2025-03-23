4ce2d392797380f1ee43a4e0ef573676ada520d3
TomBric <47101971+TomBric@users.noreply.github.com>
2025-03-23 17:17:16 +0100

    added auto flash option on radar to flash GxAirCom

--- image/mk_radar_on_stratux.sh
+++ image/mk_radar_on_stratux.sh
@@ -96,7 +96,7 @@ cd mnt/$DISPLAY_SRC || die "cd failed"
 su pi -c "git clone --recursive -b ${BRANCH} https://github.com/TomBric/stratux-radar-display.git"
 # copy T-Beam flash directory
 if [ -n "$FLASH" ]; then
-  cp -r "$FLASH" mnt/$DISPLAY_SRC/stratux-radar-display
+  cp -r "$FLASH" stratux-radar-display
   # modify stratux_radar.sh to execute flash-t-beam-once.sh
   sed -i '/\/bin\/bash/a\/bin/bash $DISPLAY_SRC/stratux-radar-display/image/flash-t-beam-once.sh $DISPLAY_SRC/stratux-radar-display/$FLASH'
 fi
