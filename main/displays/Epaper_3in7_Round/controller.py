4c6c4ce6af5abe14913cf6f01589c1c0817da068
TomBric <47101971+TomBric@users.noreply.github.com>
2025-06-09 18:42:50 +0200

    Starting Epaper_3in7_Round

--- main/displays/Epaper_3in7_Round/controller.py
+++ main/displays/Epaper_3in7_Round/controller.py
@@ -165,8 +165,8 @@ class Epaper3in7_Round(dcommon.GenericDisplay):
             t2 = ""
         if basemode:
             t2 += "\nGround\nmode"
-        self.draw.text((LEFT-20, self.SMALL + 10), t1, font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
-        self.draw.text((LEFT-30, self.SMALL+self.VERYSMALL+15), t2, font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
+        self.draw.text((LEFT-15, self.SMALL + 10), t1, font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
+        self.draw.text((LEFT-25, self.SMALL+self.VERYSMALL+15), t2, font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
 
         t = f"FL{round(ownalt / 100)}"
         textlength = self.draw.textlength(t, self.fonts[self.VERYSMALL])
