cf41708064d70d5bb32ba3319b8ca92bc12786e2
TomBric <47101971+TomBric@users.noreply.github.com>
2025-06-10 11:12:02 +0200

    Starting Epaper_3in7_Round

--- main/displays/Epaper_3in7_Round/controller.py
+++ main/displays/Epaper_3in7_Round/controller.py
@@ -255,7 +255,7 @@ class Epaper3in7_Round(dcommon.GenericDisplay):
         self.centered_text(0, f"Stratux {stat['version']}", self.SMALL)
         starty += self.SMALL + 8
         colors = {'outline': 'black', 'black_white_offset': 5}
-        bar_start, bar_end = 130, 340
+        bar_start, bar_end = 150, 370
         side_offset = 70
 
         starty = self.bar(starty, "1090", stat['ES_messages_last_minute'], stat['ES_messages_max'],
@@ -289,8 +289,8 @@ class Epaper3in7_Round(dcommon.GenericDisplay):
         self.draw.text((side_offset, starty), "altitudes", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
         alt = f"{gps_alt:5.0f}" if stat['GPS_position_accuracy'] < 19999 else " ---"
         self.draw.text((bar_start, starty), f"P-Alt {round(altitude)} ft", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
-        self.draw.text((200, starty), f"Corr {stat['AltitudeOffset']:+} ft", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
-        self.draw.text((330, starty), f"GPS-Alt {alt} ft", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
+        self.draw.text((240, starty), f"Corr {stat['AltitudeOffset']:+} ft", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
+        self.draw.text((290, starty), f"GPS-Alt {alt} ft", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
         starty += self.VERYSMALL + 5
 
         self.draw.text((side_offset, starty), "sensors", font=self.fonts[self.VERYSMALL], fill=self.TEXT_COLOR)
