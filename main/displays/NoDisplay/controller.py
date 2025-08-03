f96760742014f618dcaa91c822fb6434c20b976c
thomas <tom@tbreitbach.de>
2025-08-03 09:18:30 +0200

    Cleansing NoDisplay

--- main/displays/NoDisplay/controller.py
+++ main/displays/NoDisplay/controller.py
@@ -50,7 +50,7 @@ class NoDisplay(dcommon.GenericDisplay):
 
 
     def timer(self, utctime, stoptime, laptime, laptime_head, left_text, middle_text, right_t, timer_runs,
-              utc_color=None, timer_color=None, second_color=None):
+              utc_color=None, timer_color=None, second_color=None, datestr=None):
         pass
 
     def compass(self, heading, error_message):
