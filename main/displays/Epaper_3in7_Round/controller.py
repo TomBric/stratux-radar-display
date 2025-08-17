ba7cdb61cbf72894ae957a2c0bef915fc493a8e0
thomas <tom@tbreitbach.de>
2025-08-17 10:33:11 +0200

    distance_statistics modified

--- main/displays/Epaper_3in7_Round/controller.py
+++ main/displays/Epaper_3in7_Round/controller.py
@@ -38,7 +38,7 @@ from .. import dcommon
 from PIL import Image, ImageDraw, ImageFont
 import math
 import time
-from datetime import datetime
+import datetime
 from pathlib import Path
 import logging
 
@@ -423,7 +423,7 @@ class Epaper3in7_Round(dcommon.GenericDisplay):
         st = '---'
         if 'start_time' in values:
             dt = values['start_time']
-            if not isinstance(dt, datetime):
+            if not isinstance(dt, datetime.datetime):
                 dt = datetime.fromisoformat(dt)
             st = dt.strftime("%H:%M:%S,%f")[:-5]
         lines = [
@@ -436,7 +436,7 @@ class Epaper3in7_Round(dcommon.GenericDisplay):
         lt = '---'
         if 'landing_time' in values:
             dt = values['landing_time']
-            if not isinstance(dt, datetime):
+            if not isinstance(dt, datetime.datetime):
                 dt = datetime.fromisoformat(dt)
             lt = dt.strftime("%H:%M:%S,%f")[:-5]
         lines = [
