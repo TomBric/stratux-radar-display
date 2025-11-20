bd86bed61a404f703c0c23d837f0e3cf8f6607eb
thomas <tom@tbreitbach.de>
2025-11-20 14:00:15 +0100

    Version 2.12. Refactored globals in nearly all files for a cleaner structure

--- main/radar.py
+++ main/radar.py
@@ -599,7 +599,7 @@ async def user_interface():
             elif global_mode == Modes.STRATUX_STATUS:  # stratux status
                 stratuxstatus.start()  # starts status_listener couroutine if not yet running
                 next_mode = stratuxstatus.user_input()
-                if next_mode != 0 and next_mode != 15:
+                if next_mode != Modes.NO_CHANGE and next_mode != Modes.STRATUX_STATUS:
                     stratuxstatus.stop()  # stops status_listener
             elif global_mode == Modes.FLIGHTTIME:  # display flighttimes
                 next_mode = flighttime.user_input()
