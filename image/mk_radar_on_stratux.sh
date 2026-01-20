e0428c7db3617f2210d84fe3f3ee4b5228d36e29
thomas <tom@tbreitbach.de>
2026-01-20 10:28:27 +0100

    timeout for position introduced. POSITION_VALID_DELTA. This implements a swich back to Mode-S after 10 seconds if no further position was received.

--- image/mk_radar_on_stratux.sh
+++ image/mk_radar_on_stratux.sh
@@ -19,7 +19,7 @@
 #   sudo /bin/bash mk_radar_on_stratux.sh -s
 
 
-set -x
+# set -x
 TMPDIR="/home/pi/image-tmp"
 DISPLAY_SRC="home/pi"
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
