a3c9944806741e19066b9fb48cb3523a204b4c65
thomas <tom@tbreitbach.de>
2026-02-25 18:46:54 +0100

    Starting color mapping for Epaper_3in7

--- main/displays/Epaper_3in7/controller.py
+++ main/displays/Epaper_3in7/controller.py
@@ -58,6 +58,20 @@ class Epaper3in7(dcommon.GenericDisplay):
     ARCPOSITION_EXCLUDE_TO = 250
     ANGLE_OFFSET = 270  # offset for calculating angles in displays
 
+    PRIORITY_MAPPING_LIGHT = {
+        0: ("white", "black", 3),  # unclear
+        1: ("black", "black", 2),  # RA (Resolution Advisory)
+        2: ("white", "black", 4),  # TA (Traffic Advisory)
+        3: ("white", "black", 2),  # potential_collision
+        4: ("white", "black", 1)  # no_collision
+    }
+    PRIORITY_MAPPING_DARK = {
+        0: ("white", "black", 3),  # unclear
+        1: ("white", "white", 2),  # RA (Resolution Advisory)
+        2: ("black", "white", 4),  # TA (Traffic Advisory)
+        3: ("white", "black", 2),  # potential_collision
+        4: ("white", "black", 1)  # no_collision
+    }
 
     def __init__(self):
         super().__init__()
