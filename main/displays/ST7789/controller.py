77fbf13e055b4a9d371fccb976f0e84076e3dd13
thomas <tom@tbreitbach.de>
2026-02-25 13:53:19 +0100

    Simulation Mode for aircraft added for testing and demo purposes

--- main/displays/ST7789/controller.py
+++ main/displays/ST7789/controller.py
@@ -57,7 +57,7 @@ class ST7789(dcommon.GenericDisplay):
     PRIORITY_MAPPING_LIGHT = {
         0: ("white", "darkgray", 1),  # unclear
         1: ("red", "black", 2),  # RA (Resolution Advisory)
-        2: ("yellow", "yellow", 2),  # TA (Traffic Advisory)
+        2: ("yellow", "black", 2),  # TA (Traffic Advisory)
         3: ("black", "black", 1),  # potential_collision
         4: ("lightgreen", "lightgreen", 1)  # no_collision
     }
