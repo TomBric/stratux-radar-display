cd58a4aa951ee423280a16c15e0e45832beeec2c
thomas <tom@tbreitbach.de>
2026-05-03 20:13:21 +0200

    mk_radar_on_stratux.sh modified to use version 2.0pre if wanted

--- image/configure_radar_on_stratux.sh
+++ image/configure_radar_on_stratux.sh
@@ -28,9 +28,10 @@ apt update
 apt install git python3-pip -y
 # no sound or bluetooth on stratux
 # apt install pipewire pipewire-audio pipewire-alsa libspa-0.2-bluetooth libttspico-utils
-apt install pipewire pipewire-audio pipewire-alsa libspa-0.2-bluetooth python3-alsaaudio -y
-apt install python3-websockets python3-xmltodict python3-pydbus python3-luma.oled python3-pip python3-numpy python3-pygame -y
+apt install pipewire pipewire-audio pipewire-alsa libspa-0.2-bluetooth python3-alsaaudio libttspico-utils -y
+apt install python3-websockets python3-xmltodict python3-pydbus python3-luma.oled python3-pip python3-numpy python3-pygame python3-numpy python3-scipy python3-gpiozero -y
 su pi -c "pip3 install  ADS1x15-ADC --break-system-packages"
+su pi -c "pip3 install bayesian-filters --break-system-packages"
 /bin/bash "$(dirname "$0")"/mk_config_webapp.sh -s
 
 # check to get sound running on stratux
