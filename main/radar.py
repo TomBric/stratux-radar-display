266bb590e8b51d0644c16222f33ee8968dc244db
thomas <tom@tbreitbach.de>
2026-05-20 07:50:00 +0200

    Logging for COLLISION_DETECT added

--- main/radar.py
+++ main/radar.py
@@ -403,7 +403,7 @@ def collision_detection(ac, traffic, mode_s=False):
     if old_prio in [1, 2] and not ac['prio'] in [1, 2]:  # there was a RA or TA on this aircraft, now it's clear
         # check if there is still another RA situation
         if check_clear_of_traffic():
-            rlog.log(COLLISION_DEBUG, f"Clear of conflict: {ac['Icao_addr']} {ac['prio']} {ac['hdiff']} {ac['gps_distance']} {ac['gps_angle']} {ac['course']} {ac['prio']}")
+            rlog.log(COLLISION_DEBUG, f"Clear of conflict: {ac.get('tail',"unknown")} ")
             radarbluez.priority_speak("Clear of conflict", 130)
 
 
