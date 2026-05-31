fa1e0c3fd9ab606b609c8afebf5484bb63a1b949
thomas <tom@tbreitbach.de>
2026-05-31 16:53:42 +0200

    Added own velocity indicator

--- main/radar.py
+++ main/radar.py
@@ -71,7 +71,7 @@ import sys
 import traceback
 import syslog
 
-from globals import rlog, Globals, Modes, global_config, SITUATION_DEBUG, AIRCRAFT_DEBUG, COLLISION_DEBUG, SPEED_ARROW_TIME
+from globals import rlog, Globals, Modes, global_config, SITUATION_DEBUG, AIRCRAFT_DEBUG, COLLISION_DEBUG
 
 # constants
 RADAR_VERSION = "3.00"
