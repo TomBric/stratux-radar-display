464b9ee826f1497c8ef7b6ec83e2f3064c8e2a59
thomas <tom@tbreitbach.de>
2026-05-31 17:07:45 +0200

    Added own velocity indicator

--- main/displays/Epaper_3in7_Round/controller.py
+++ main/displays/Epaper_3in7_Round/controller.py
@@ -212,8 +212,8 @@ class Epaper3in7_Round(dcommon.GenericDisplay):
 
         if gps_speed_length > 0:    # draw own speed vector
             velocity_width = max(2, self.AIRCRAFT_SIZE // 3)
-            self.draw.rectangle((self.zerox - velocity_width, self.zeroy, self.zerox + velocity_width,
-                            self.zeroy - gps_speed_length), fill=self.TEXT_COLOR)
+            self.draw.rectangle((self.zerox - velocity_width, self.zeroy - gps_speed_length, self.zerox + velocity_width,
+                            self.zeroy), fill=self.TEXT_COLOR)
         # range
         self.draw.text((LEFT, 1), f"{rrange} nm", font=self.fonts[self.SMALL], fill=self.TEXT_COLOR)
 
