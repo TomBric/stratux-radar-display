2a5f0e5d06cc22876b7a0ddf9571a770cbc244a6
thomas <tom@tbreitbach.de>
2026-09-04 13:59:17 +0200

    Skript modified for bookworm

--- image/mk_build_stratux.sh
+++ image/mk_build_stratux.sh
@@ -49,14 +49,15 @@ else
 fi
 
 # start with a copy of the base image, so we don't modify the original
-cp -f "$BASE_IMGNAME" "$IMGNAME" || die "Creating working image copy failed"
+cp -vf "$BASE_IMGNAME" "$IMGNAME" || die "Creating working image copy failed"
 
 echo "Bookworm arm64 lite image prepared at $IMGNAME"
 # Check where in the image the root partition begins:
-bootoffset=$(parted $IMGNAME unit B p | grep fat32 | awk -F ' ' '{print $2}')
-bootoffset=${bootoffset::-1}
-partoffset=$(parted $IMGNAME unit B p | grep ext4 | awk -F ' ' '{print $2}')
-partoffset=${partoffset::-1}
+bootoffset=$(parted "$IMGNAME" unit B p | awk '$5 == "fat32" { print substr($2, 1, length($2) - 1); exit }')
+partoffset=$(parted "$IMGNAME" unit B p | awk '$5 == "ext4" { print substr($2, 1, length($2) - 1); exit }')
+
+[ -n "$bootoffset" ] || die "Boot partition offset not found in $IMGNAME"
+[ -n "$partoffset" ] || die "Root partition offset not found in $IMGNAME"
 
 echo "Boot partition starts at $bootoffset bytes, root partition starts at $partoffset bytes"
 
