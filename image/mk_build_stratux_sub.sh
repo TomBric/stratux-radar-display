5fbd13d64150e478050b235dced74c45ecb5ed7b
thomas <tom@tbreitbach.de>
2026-09-04 15:42:13 +0200

    Skript modified for bookworm

--- image/mk_build_stratux_sub.sh
+++ image/mk_build_stratux_sub.sh
@@ -157,38 +157,43 @@ systemctl disable dphys-swapfile
 apt purge dphys-swapfile -y
 
 # purge list from stratux image_build/stage2/10-stratux/files/purge-list.txt
-purge -y cifs-utils
-apt purge -y eject
-apt purge -y gcc g++
-apt purge -y git git-man
-apt purge -y libfontconfig1
-apt purge -y libfreetype6
-apt purge -y gdb
-apt purge -y ntfs-3g
-apt purge -y xauth
-apt purge -y cpp cpp-12
-apt purge -y ed
-apt purge -y libqt5core5a
-apt purge -y libsource-highlight-common libsource-highlight4v5
-apt purge -y libtiff6
-apt purge -y strace
-apt purge -y v4l-utils
-apt purge -y apparmor
-apt purge -y ncdu
-apt purge -y libc6-dev
-apt purge -y pkgconf
-apt purge -y python3-apt
-apt purge -y python3-colorzero
-apt purge -y udisks2
-apt purge -y xkb-data
-apt purge -y m4
-apt purge -y make
-apt purge -y nfs-common
-apt purge -y ppp
-apt purge -y fbset
-apt purge -y manpages
-apt purge -y manpages-dev
-apt purge -y man-db
+apt purge -y \
+  cifs-utils \
+  eject \
+  gcc \
+  g++ \
+  git \
+  git-man \
+  libfontconfig1 \
+  libfreetype6 \
+  gdb \
+  ntfs-3g \
+  xauth \
+  cpp \
+  cpp-12 \
+  ed \
+  libqt5core5a \
+  libsource-highlight-common \
+  libsource-highlight4v5 \
+  libtiff6 \
+  strace \
+  v4l-utils \
+  apparmor \
+  ncdu \
+  libc6-dev \
+  pkgconf \
+  python3-apt \
+  python3-colorzero \
+  udisks2 \
+  xkb-data \
+  m4 \
+  make \
+  nfs-common \
+  ppp \
+  fbset \
+  manpages \
+  manpages-dev \
+  man-db
 
 #
 
